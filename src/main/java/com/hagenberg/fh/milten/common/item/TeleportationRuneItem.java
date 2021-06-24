844a9cf2977b78f7c2defffb4d8e6863a1036335
Frazerus <s1910238027@students.fh-hagenberg.at>
2021-06-24 20:53:44 +0200

    Added first algorithm to find the center lapis corners and the surrounding corners of where the chiseled stone bricks should be

--- src/main/java/com/hagenberg/fh/milten/common/item/TeleportationRuneItem.java
+++ src/main/java/com/hagenberg/fh/milten/common/item/TeleportationRuneItem.java
@@ -1,8 +1,7 @@
 package com.hagenberg.fh.milten.common.item;
 
-import com.hagenberg.fh.milten.common.block.TeleportationPlatformBlock;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -17,10 +16,16 @@ import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class TeleportationRuneItem extends Item {
 
+    private Logger out = Logger.getLogger("SEARCHCORNERS");
     boolean oneUse;
+    private final int maxSize = 15;
+
+    private int maxX;
+    private boolean set = false;
 
     public TeleportationRuneItem(boolean _oneUse) {
         super(new Item.Properties().group(ItemGroup.TRANSPORTATION).maxStackSize(_oneUse ? 64 : 1));
@@ -46,7 +51,12 @@ public class TeleportationRuneItem extends Item {
             //tags.putInt("dim", 0); //getDimension dose not work with player and or world
             tags.putFloat("direction", playerIn.rotationYaw);
 
+
+
+
+
             if (!world.isRemote) {
+                createPlatform(offPos,world,playerIn);
                 playerIn.sendStatusMessage(new StringTextComponent(TextFormatting.GREEN + "This crystal is linked at:" + " " +
                         tags.getInt("x") + ", " +
                         tags.getInt("y") + ", " +
@@ -54,6 +64,8 @@ public class TeleportationRuneItem extends Item {
             }
 
         }
+
+
 
         return ActionResultType.func_233537_a_(world.isRemote);
     }
@@ -114,5 +126,102 @@ public class TeleportationRuneItem extends Item {
                     TextFormatting.GREEN + "Linked: " + tags.getInt("x") + ", " +
                             tags.getInt("y") + ", " + tags.getInt("z")));
         }
+    }
+
+    /**
+     * searches for the north west and south east corner of the platform
+     * @return an empty array if the blocks dont match, else the 2 corners
+     */
+    private void createPlatform(BlockPos start, World worldIn,PlayerEntity PlayerIn){
+        if(!worldIn.isRemote){
+            if (checkLapis(start,worldIn)){
+                //finds the corners of the lapis rect
+                BlockPos NorthWestL = searchForLapisCorners(start,worldIn, -1, -1);
+                BlockPos SouthEastL = searchForLapisCorners(start,worldIn,1,1);
+                out.info("NW: " + NorthWestL.toString());
+                out.info("SE: " + SouthEastL.toString());
+                 BlockPos [] Corners = createCorners(NorthWestL, SouthEastL,worldIn);
+                 if(Corners == null){
+                     PlayerIn.sendStatusMessage(new StringTextComponent(TextFormatting.RED +
+                             "The Platform needs to be a solid Rectangle surrounded with Chiseled Stone Bricks")
+                             ,true);
+                     return;
+                 }
+            }
+            PlayerIn.sendStatusMessage(new StringTextComponent(TextFormatting.RED +
+                    "Rightclick a rectangular Platform made of Lapis Blocks in the middle, surrounded on all sides by Chiseled Stone Bricks to create a Teleportation Platform")
+                    ,true);
+        }
+    }
+
+
+    private BlockPos searchForLapisCorners(BlockPos toCheck, World worldIn, int up, int left){
+        if(set && toCheck.getX() >= maxX && left > 0 ){
+            /*out.info("/////// Tocheck X is too big" + toCheck.toString());
+            out.info("maxX: "+ maxX);
+            out.info("left: " + left);
+             */
+            return null;
+        }
+        else if(set && toCheck.getX() <= maxX && left < 0){
+            /*out.info("|||||||| Tocheck X is too small" + toCheck.toString());
+            out.info("maxX: "+ maxX);
+            out.info("left: " + left);
+
+             */
+            return null;
+        }
+        //out.info("Checking at: " + toCheck.toString());
+        if(checkLapis(toCheck,worldIn)){
+            BlockPos checked = searchForLapisCorners(toCheck.add(left,0,0),worldIn,up,left);
+            if(checked == null){
+                if(!set) {
+                    set = true;
+                    maxX = toCheck.getX() + left;
+                    //out.info("MAX SET TO: "+maxX);
+                }
+                checked = searchForLapisCorners(toCheck.add(0,0,up),worldIn,up,left);
+
+                if(checked==null){
+                    set = false;
+                    //out.info("#*#*#*# final found:" + toCheck.toString());
+                    return toCheck;
+                }
+                else {
+
+                    //out.info("####### final found and passing z:" + checked.toString());
+                    return checked;
+
+                }
+            }
+            else {
+                //out.info("****** final found and passing x:" + checked.toString());
+                return checked;
+            }
+        }
+        //out.info("no things found at: "+ toCheck.toString());
+        return null;
+    }
+
+    private boolean checkLapis(BlockPos pos, World worldIn){
+        if(worldIn.getBlockState(pos).getBlock().equals(Blocks.LAPIS_BLOCK)){
+            return true;
+        }
+        return false;
+    }
+
+    private BlockPos[] createCorners(BlockPos nw, BlockPos se,World worldIn){
+        BlockPos [] out = {
+                nw.add(-1,0,-1),
+                new BlockPos(se.getX()+1,nw.getY(),nw.getZ()+1),
+                se.add(1,0,1),
+                new BlockPos(nw.getX()-1,nw.getY(),se.getZ()-1)
+        };
+        for(BlockPos pos : out){
+            if (!worldIn.getBlockState(pos).getBlock().equals(Blocks.CHISELED_STONE_BRICKS)){
+                return null;
+            }
+        }
+        return out;
     }
 }
